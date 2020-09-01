96dbc2b85666aa82a7020748f2a13b506614e3d8
Andre Lee <andreleecoder@gmail.com>
2020-09-01 13:38:34 -0300

    adicionando efeito ao header e mudando positon

--- challenge/dist/index.js
+++ challenge/dist/index.js
@@ -1,44 +1,57 @@
-const gtapi = "https://api.github.com/users/andreleecoder"
-const ul = document.getElementById('repo-list')
+const gtapi = 'https://api.github.com/users/andreleecoder';
+const ul = document.getElementById('repo-list');
 
 //Create element
 function createNode(element) {
-	return document.createElement(element)
+  return document.createElement(element);
 }
 
 //Append element to it's parent
 function append(parent, el) {
-	return parent.appendChild(el)
+  return parent.appendChild(el);
 }
+//change header background
+
+(function () {
+  var menu = document.getElementById('header'); // colocar em cache
+  window.addEventListener('scroll', function () {
+    if (window.scrollY > 0) menu.classList.add('--headerBg');
+    // > 0 ou outro valor desejado
+    else menu.classList.remove('--headerBg');
+  });
+})();
 
 //Get the list of user's repositories
 async function getRepos(gtapi) {
-	ul.innerHTML=''
-	await fetch(gtapi)
-		.then(resp => resp.json())
-		.then(response => {
-			return response.map(function(repo) {
-				let li = createNode('li')
-				li.innerHTML = repo.name
-				append(ul, li)
-			})
-		})
-		.catch(function(error) {
-			console.log(error)
-		})
-	document.querySelector('.repo-block')
+  ul.innerHTML = '';
+  await fetch(gtapi)
+    .then((resp) => resp.json())
+    .then((response) => {
+      return response.map(function (repo) {
+        let li = createNode('li');
+        li.innerHTML = repo.name;
+        append(ul, li);
+      });
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
+  document.querySelector('.repo-block');
 }
 
 //Show User's Data
- async function fetchData (gtapi) {
-	let userData;
-	 await fetch(gtapi)
-	.then(resp => resp.json())
-	.then(data => (userData = data))
-	.then(() => {
-		document.querySelector(".repos").innerHTML = 'Repositórios: ' + userData.public_repos
-		document.querySelector(".flwrs").innerHTML = 'Seguidores: ' + userData.followers
-		document.querySelector(".flwn").innerHTML = 'Seguindo: ' + userData.following
-	})
+async function fetchData(gtapi) {
+  let userData;
+  await fetch(gtapi)
+    .then((resp) => resp.json())
+    .then((data) => (userData = data))
+    .then(() => {
+      document.querySelector('.repos').innerHTML =
+        'Repositórios: ' + userData.public_repos;
+      document.querySelector('.flwrs').innerHTML =
+        'Seguidores: ' + userData.followers;
+      document.querySelector('.flwn').innerHTML =
+        'Seguindo: ' + userData.following;
+    });
 }
 fetchData(gtapi);
